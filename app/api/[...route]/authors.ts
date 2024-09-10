d14747c4d7d98eeb0da822481900f6c35554ac44
sajidmahamud835 <sajidmahamud835@gmail.com>
2024-09-11 01:42:46 +0600

    removed useless codes

--- app/api/[...route]/authors.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const app = new Hono();
-
-app.get("/", (c) => c.json("List authors"));
-app.post("/", (c) => c.json("Create an author", 201));
-app.get("/:id", (c) => c.json(`Get ${c.req.param("id")}`));
-
-export default app;
